f5a339910c096224779d7ea75fc63a0cda5deded
yuanxx <ysswork@163.com>
2018-02-14 03:35:18 +0800

    Update getBirthday.js

--- app/js/getBirthday.js
+++ app/js/getBirthday.js
@@ -4,8 +4,8 @@ export function compute() {
   let y = date.getFullYear()
   let m = date.getMonth()
   let d = date.getDate()
-  if (y === 2017 && m === 11 && d === 20) {
-    return 23
+  if (y === 2018 && m === 03 && d === 28) {
+    return 25
   }
   return false
 }
